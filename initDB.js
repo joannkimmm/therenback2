df8f78443043a1e7ef3ec6a45ac11b11da776fc3
inspirAZN <jcaluza@ucsd.edu>
2014-02-21 01:59:41 -0800

    initdb and json files

--- initDB.js
+++ initDB.js
@@ -16,7 +16,7 @@ var models   = require('./models');
 
 // Connect to the Mongo database, whether locally or on Heroku
 // MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
-var local_database_name = 'lab7';
+var local_database_name = 'therenback';
 var local_database_uri  = 'mongodb://localhost/' + local_database_name
 var database_uri = process.env.MONGOLAB_URI || local_database_uri
 mongoose.connect(database_uri);
@@ -25,10 +25,10 @@ mongoose.connect(database_uri);
 // Do the initialization here
 
 // Step 1: load the JSON data
-var projects_json = require('./projects.json');
+var moments_json = require('./moments.json');
 
 // Step 2: Remove all existing documents
-models.Project
+models.Moments
   .find()
   .remove()
   .exec(onceClear); // callback to continue at
@@ -40,9 +40,9 @@ function onceClear(err) {
   // loop over the projects, construct and save an object from each one
   // Note that we don't care what order these saves are happening in...
   var to_save_count = projects_json.length;
-  for(var i=0; i<projects_json.length; i++) {
-    var json = projects_json[i];
-    var proj = new models.Project(json);
+  for(var i=0; i<moments_json.length; i++) {
+    var json = moments_json[i];
+    var proj = new models.Moments(json);
 
     proj.save(function(err, proj) {
       if(err) console.log(err);
