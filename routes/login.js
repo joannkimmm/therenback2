8a62b6d93d79f83c563db1da2dd450f2c0eef862
inspirAZN <jcaluza@ucsd.edu>
2014-02-20 22:11:34 -0800

    Trying login again again

--- routes/login.js
+++ routes/login.js
@@ -7,13 +7,5 @@ var models = require('../models');
 
 exports.view = function(req, res){
 
-	models.Project
-		.find()
-		.sort('date')
-		.exec(renderProjects);
-
-	function renderProjects(err, projects) {
-		res.render('index', { 'projects': projects });
-	}
 
 };
