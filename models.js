0a16adebeb657e5e456337a656d3da4126d18b99
Joann Kim <jnk006@ucsd.edu>
2014-02-27 19:12:22 -0800

    changes for login

--- models.js
+++ models.js
@@ -8,7 +8,6 @@ var MomentsSchema = new Mongoose.Schema({
   "imgsrc": String,
   "numPhotos": Number,
   "heart": String
-
 });
 exports.Moments = Mongoose.model('Moments', MomentsSchema);
 
@@ -19,7 +18,6 @@ var myMomentsSchema = new Mongoose.Schema({
   "imgsrc": String,
   "numPhotos": Number,
   "heart": String
-
 });
 exports.myMoments = Mongoose.model('myMoments', myMomentsSchema);
 
@@ -32,54 +30,3 @@ var FavoritesSchema = new Mongoose.Schema({
   "heart": String
 });
 exports.Favorites = Mongoose.model('Favorites', FavoritesSchema);
-
-// Model: User
-
-User = new Schema({
-  'username': { type: String, validate: [validatePresenceOf, 'a username is require'], index: {unique:true}},
-  'hashed_password': String,
-  'salt': String
-});
-
-User.virtual('id')
-  .get(function() {
-    return this._id.toHexString();
-  });
-
-User.virtual('password')
-  .set(function(password){
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password);
-  })
-  .get(function() { return this._password });
-
-
-User.method('authenticate', function(plainText){
-  return this.encryptPassword(plainText) === this.hashed_password;
-});
-
-User.method('makeSalt', function() {
-  return Math.round((new Date().valueOf() * Math.random())) + '';
-});
-
-User.method('encryptPassword', function(password) {
-  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-});
-
-User.pre('save', function(next) {
-  if (!validatePresenceOf(this.password)) {
-    next(new Error('Invalid password'));
-  } else {
-    next();
-  }
-});
-
-mongoose.model('User', User);
-
-fn();
-
-
-
-
-
